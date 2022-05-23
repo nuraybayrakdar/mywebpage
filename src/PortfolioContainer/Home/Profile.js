d18c3b78653e3227855f5f26a9a4c025f24bc670
nuraybayrakdar <bayrakdarnuray@gmail.com>
2022-05-23 15:13:57 +0300

    profile container design

--- src/PortfolioContainer/Home/Profile.js
+++ src/PortfolioContainer/Home/Profile.js
@@ -1,24 +1,28 @@
 import React from "react";
 import Typical from "react-typical";
+import "./Profile.css";
 
 export default function Profile() {
   return (
     <div className="profile-container">
+      <meta name="viewport" content="width-device-width, initial-scale=1 maximum-scale=1" ></meta>
       <div className="profile-parent">
         <div className="profile-details">
           <div className="colz">
-            <a href="https://github.com/nuraybayrakdar">
-              <i className="fa fa-github"></i>
-            </a>
-            <a href="#">
-              <i className="fa fa-twitter"></i>
-            </a>
-            <a href="https://www.linkedin.com/in/nuray-bayrakdar/">
-              <i className="fa fa-linkedin"></i>
-            </a>
-            <a href="https://www.instagram.com/nnurayb/">
-              <i className="fa fa-instagram"></i>
-            </a>
+            <div className="colz-icon">
+              <a href="https://github.com/nuraybayrakdar">
+                <i className="fa fa-github"></i>
+              </a>
+              <a href="#">
+                <i className="fa fa-twitter"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/nuray-bayrakdar/">
+                <i className="fa fa-linkedin"></i>
+              </a>
+              <a href="https://www.instagram.com/nnurayb/">
+                <i className="fa fa-instagram"></i>
+              </a>
+            </div>
           </div>
           <div className="profile-details-name">
             <span className="primary-text">
@@ -26,6 +30,7 @@ export default function Profile() {
               Hello, I'M <span className="highlighted-text">Nuray</span>
             </span>
           </div>
+
           <div className="profile-details-role">
             <span className="primary-text">
               {" "}
@@ -33,11 +38,18 @@ export default function Profile() {
                 {" "}
                 <Typical
                   loop={Infinity}
-                  steps={["EAT SLEEP CODE REPEAT", 1000]}
+                  steps={[
+                    "EAT SLEEP CODE REPEAT",
+                    1000,
+                    "EAT SLEEP CODE REPEAT",
+                    1000,
+                    "EAT SLEEP CODE REPEAT",
+                    1000,
+                  ]}
                 />
               </h1>
               <span className="profile-role-tagline">
-                On the way becoming a developer.
+                Building applications with frontend and backend operations.
               </span>
             </span>
           </div>
@@ -46,13 +58,15 @@ export default function Profile() {
               {""}
               Hire me{" "}
             </button>
-            <a href="#" dowland="cv"></a>
-            <button className="btn highlighted-btn">
-              Get Resume
-            </button>
+            <a href="#" dowland="cv">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
           </div>
         </div>
       </div>
+      <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
     </div>
   );
 }
